881167f686fab18a3cafff34d7cf6f1b19b302dc
Danish Faisal <danishfaisal.wwe@gmail.com>
2022-10-06 21:27:30 +0530

    Setting up Apollo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import "./App.css";
+import Home from "./pages/Home";
+
+const client = new ApolloClient({
+  uri: "https://graphql-weather-api.herokuapp.com/",
+  cache: new InMemoryCache(),
+});
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <ApolloProvider client={client}>
+      <div className="App">
+        <Home />
+      </div>
+    </ApolloProvider>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div className="home">
+      <h1>Search For Weather</h1>
+      <input type="text" placeholder="Enter a City Name..." />
+      <button>Search</button>
+    </div>
+  );
+}
+
+export default Home;
